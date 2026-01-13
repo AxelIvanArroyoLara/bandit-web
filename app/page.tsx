d72f4bea4448afcd3d7ddbe06be64fa6a82473ed
BTG47 <btg151205@hotmail.com>
2026-01-12 22:39:42 -0600

    NavBar now appears in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 'use client'
+import Navbar from './components/Navbar'
 import ProductCard from './components/ProductCard'
 import HeroSection from './components/HeroSection'
 import ProductGrid from './components/ProductGrid'
 import ProductGrid2 from './components/ProductGrid2'
 import ProductGrid3 from './components/ProductGrid3'
 import ProductGrid4 from './components/ProductGrid4'
-import { PERFUMES_DATA } from './data/perfumes' // Importa desde data
+import { PERFUMES_DATA } from './data/perfumes'
 
 export default function Home() {
   return (
     <div className="bg-gta-black min-h-screen">
+      <Navbar/>
       <HeroSection />
       <ProductGrid />
       <ProductGrid2 />
